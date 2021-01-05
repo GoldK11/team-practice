456544f6bb92fe8da84d49cb95836378ec06cfff
GoldK11 <ssori113@gmail.com>
2021-01-05 18:58:19 +0900

    결 change

--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Body() {
-  return <div className="Body" />;
+  return <div className="Body">body 영역</div>;
 }
 
 export default Body;
