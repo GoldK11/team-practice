98a282d459046a502b101fffdf38dc85d5141226
JIYEONGSTAR <jiyeongstar@gmail.com>
2021-01-05 18:49:27 +0900

    지영change

--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 function Body() {
-  return <div className="Body" />;
+  return (<div className="Body" >
+  <p>박지영 파이팅 김결 파이팅 이은학은 망해라</p>
+  </div>
+  );
 }
 
 export default Body;
